defb23ce207f80066238d9dd740473707960fcbc
Patrick Read <pread13@gmail.com>
2014-10-28 16:35:41 -0400

    Initial Commit

---
+++ stripetest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  stripetest
+//
+//  Created by Patrick Read on 10/28/14.
+//  Copyright (c) 2014 Beam Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ stripetest/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  stripetest
+//
+//  Created by Patrick Read on 10/28/14.
+//  Copyright (c) 2014 Beam Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
