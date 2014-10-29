9fb33155a2901b8eba997d6b15879a407dcd68e5
Patrick Read <pread13@gmail.com>
2014-10-29 08:35:03 -0400

    added a payment view controller

---
+++ stripetest/PaymentViewController.h
@@ -0,0 +1,13 @@
+//
+//  PaymentViewController.h
+//  stripetest
+//
+//  Created by Patrick Read on 10/29/14.
+//  Copyright (c) 2014 Beam Technologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PaymentViewController : UIViewController
+
+@end
